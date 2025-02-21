ec13848c81aa79fea36b4dc4fa325cd032d4d938
John S Weeks <steve@offthekitchen.com>
2025-02-20 21:15:43 -0700

    Added  function to get distinct breweries

--- src/composables/breweries.ts
+++ src/composables/breweries.ts
@@ -1,31 +1,38 @@
-import { reactive, onMounted } from "vue"
-import jsonBreweries from "../data/BREWERIES.json"
+import { ref, reactive } from "vue"
 import type { IBrewery } from "../interfaces/brewery"
 import type { IPerformance } from "../interfaces/performance";
 import type { IStatistic } from "../interfaces/statistic"
+import jsonBreweries from "../data/BREWERIES.json"
+
 
 export function useBreweries() {
-  var breweryPerformances = reactive<IBrewery[]>(jsonBreweries.breweries)
 
-    // filter list of cities to those performed in
-    function getDistinctBreweries(performances: IPerformance[]) {
-      //Need filtering code here
-      performances.forEach(performance => {
-          if(performance.venue.includes('Brew')) {
-            breweryPerformances.push({brewery: performance.venue, cityName: performance.cityName, stateCode: performance.stateCode})
-          }
-      })
+  var breweryPerformances = ref<IBrewery[]>(jsonBreweries.breweries)
+
+  // Build a list of distinct breweries I've performed in
+  function getDistinctBreweries(performances: IPerformance[]) {
+    let distinctBreweryPerformances: IBrewery[] = [] 
+    performances.forEach(performance => {
+        if (performance.venue.includes('Brew') && !distinctBreweryPerformances.some(perf => perf.brewery === performance.venue)){
+            distinctBreweryPerformances.push({brewery: performance.venue, cityName: performance.cityName, stateCode: performance.stateCode})
+        }
+      }
+    )
+
+    if(distinctBreweryPerformances.length > 0) {
+      breweryPerformances.value = distinctBreweryPerformances
     }
+  }
 
-   // determine number of states performed in for brewery performances
+   // Build and array of distinct US states performed in at breweries
    function getDistinctStates() {
     const seenValues = new Set()
-    return breweryPerformances.filter(obj => {
-      const value = obj['stateCode']
-      if (seenValues.has(value)) {
+    return breweryPerformances.value.filter(performance => {
+      const stateCode = performance['stateCode']
+      if (seenValues.has(stateCode)) {
         return false
       }
-      seenValues.add(value)
+      seenValues.add(stateCode)
       return true
     })
   }
@@ -33,7 +40,7 @@ export function useBreweries() {
     function getStats() {
       let distinctStates = getDistinctStates()
       let stats: IStatistic[] = []
-      stats.push({stat: 'Breweries Performed in:', statValue: breweryPerformances.length.toLocaleString()})
+      stats.push({stat: 'Breweries Performed in:', statValue: breweryPerformances.value.length.toLocaleString()})
       stats.push({stat: 'Number of States:', statValue: distinctStates.length.toLocaleString()})
      return stats
     }
